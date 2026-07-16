27d3555d62b6fc770997ea0ccd114d7f44c83c45
Rashmi <rashmiramasamy07@gmail.com>
2026-07-16 10:51:18 +0530

    add changes

---
+++ Arrays/2D/simpleinp.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main()
+{
+    int rows, cols;
+
+    // Get the size of the matrix
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+
+    printf("Enter number of columns: ");
+    scanf("%d", &cols);
+
+    int matrix[rows][cols];
+
+    // Input matrix elements
+    printf("Enter the matrix elements:\n");
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    // Print the matrix
+    printf("\nThe matrix is:\n");
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
